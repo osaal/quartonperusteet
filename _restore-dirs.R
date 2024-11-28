657c2d86006d48a47ad122624519ee286b93beef
Oliver Saal <29450366+osaal@users.noreply.github.com>
2024-11-28 17:55:07 +0200

    Add basic structure of book -  Added seven placeholder appendices -  Rewritten index.qmd -  Added manual versioning -  Added freezing by default -  Added MIT license -  Cleaned up post-render script debug messages

--- _restore-dirs.R
+++ _restore-dirs.R
@@ -1,16 +1,11 @@
 ### POST-RENDER
 # Find .qmd files to move back into original paths
-print("Started post-render script")
 moved_files <-
   fs::dir_ls(type = "file") |>
   grep("^(?!index\\.qmd$).+\\.qmd$", x = _, perl = TRUE, value = TRUE)
-print("Found all moved files")
 
 # Move files into original paths
 orig_paths <- read.csv("orig_files.csv")[,1]
-print("Read in original file paths")
-print(paste0("Original files: ", orig_paths))
-print(paste0("Moved files: ", moved_files))
 lapply(
   moved_files,
   function(file) {
